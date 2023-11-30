814cc4c02c27ce1b159b748b3b047b51f00176db
KyleZrey <kyleandre.castro.cics@ust.edu.ph>
2023-11-30 21:05:24 +0800

    separated scripts for cleaning enterprise dept files

---
+++ scripts/clean_merchant_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df_merchant_data = pd.read_html('/mnt/c/Users/araza/Documents/1/git repos/ShopZada/Project Dataset/Enterprise Department/merchant_data.html')
+
+#Remove Unnamed: 0 column
+df_merchant_data = df_merchant_data.drop('Unnamed: 0', axis=1)
+
+#Remove duplicates based on merchant_id, keep first instance based on creation_date
+df_merchant_data = df_merchant_data.sort_values(by=['merchant_id', 'creation_date']).drop_duplicates(subset=['merchant_id'], keep='first')
+
+#Standardize contact_number
+df_merchant_data['contact_number'] = df_merchant_data['contact_number'].str.replace('\.', '-', regex=True)
+df_merchant_data['contact_number'] = df_merchant_data['contact_number'].str.replace('[^0-9+()-]', '', regex=True)
+
+#Title case for address except for country
+df_merchant_data['street'] = df_merchant_data['street'].str.title()
+df_merchant_data['state'] = df_merchant_data['state'].str.title()
+df_merchant_data['city'] = df_merchant_data['city'].str.title()
+
+df_merchant_data.to_parquet('/mnt/c/Users/araza/Documents/1/git repos/ShopZada/exports/clean_merchant_data.parquet')
---
+++ scripts/clean_staff_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df_staff_data = pd.read_html('/mnt/c/Users/araza/Documents/1/git repos/ShopZada/Project Dataset/Enterprise Department/staff_data.html')
+
+#Remove Unnamed: 0 column
+df_staff_data = df_staff_data.drop('Unnamed: 0', axis=1)
+
+#Remove duplicates based on staff_id, keep first instance based on creation_date
+df_staff_data = df_staff_data.sort_values(by=['staff_id', 'creation_date']).drop_duplicates(subset=['staff_id'], keep='first')
+
+#Standardize contact_number
+df_staff_data['contact_number'] = df_staff_data['contact_number'].str.replace('\.', '-', regex=True)
+df_staff_data['contact_number'] = df_staff_data['contact_number'].str.replace('[^0-9+()-]', '', regex=True)
+
+#Standardize strings, use title case except for country
+df_staff_data['name'] = df_staff_data['name'].str.title()
+df_staff_data['job_level'] = df_staff_data['job_level'].str.title()
+df_staff_data['street'] = df_staff_data['street'].str.title()
+df_staff_data['state'] = df_staff_data['state'].str.title()
+df_staff_data['city'] = df_staff_data['city'].str.title()
+
+df_staff_data.to_parquet('/mnt/c/Users/araza/Documents/1/git repos/ShopZada/exports/clean_staff_data.parquet')
